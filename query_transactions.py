962e8d9ff7823f231427ae9ae4e7e7b37b3cc31b
Sarah West <sarahwest@Sarahs-MacBook-Pro.local>
2020-10-02 18:23:22 -0700

    Black reformat

--- query_transactions.py
+++ query_transactions.py
@@ -1,31 +1,36 @@
 import sqlite3
 
+
 def run():
 
-	start_date = '2017-01-01'
-	end_date = '2020-01-01'
-	database_name = 'Postie.db'
-	db_connection = connect_sqllite_database(database_name)
-	db_cursor = db_connection.cursor()
-	table_name = 'postie_transactions'
+    start_date = "2017-01-01"
+    end_date = "2020-01-01"
+    database_name = "Postie.db"
+    db_connection = connect_sqllite_database(database_name)
+    db_cursor = db_connection.cursor()
+    table_name = "postie_transactions"
 
-	total_sales = get_total_sales(db_cursor, table_name, start_date, end_date)[0]
-	print(f'Total sales from {start_date} to {end_date} is ${total_sales}')
+    total_sales = get_total_sales(db_cursor, table_name, start_date, end_date)[0]
+    print(f"Total sales from {start_date} to {end_date} is ${total_sales}")
+
 
 def connect_sqllite_database(db_name):
-    """Creates a connection obection to a local database file, or creates a 
+    """Creates a connection obection to a local database file, or creates a
     new one with the database name as `db_name`.
     """
     return sqlite3.connect(db_name)
 
 
 def get_total_sales(db_cursor, table_name, start_date, end_date):
-    return db_cursor.execute(f'''
+    return db_cursor.execute(
+        f"""
         SELECT SUM(checkout_amount) as sales
         FROM {table_name}
         WHERE DATE(timestamp) >= DATE('{start_date}')
           AND DATE(timestamp) <= DATE('{end_date}')
-    ''').fetchone()
+    """
+    ).fetchone()
+
 
 if __name__ == "__main__":
     run()
